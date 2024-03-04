b9ca855a9e3fedaa4142110a38b909fecbb417de
Muhammad Hunain <153491211+MuhammadHunainGanatra@users.noreply.github.com>
2024-03-04 13:10:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+document.write("Result:");
+document.write("<br/>" );
+var value=("The value of a is :")
+var num=(10);
+document.write(value + num);
+document.write("<br/>" );
+document.write("--------------------------------------------");
+document.write("<br/>" );
+var second = ("The value of ++a is :");
+var secondone = ("Now the value of a is ")
+var oneresult=(++num);
+document.write(second+oneresult)
+document.write("<br/>" );
+document.write(secondone+oneresult)
+document.write("<br/>" );
+document.write("<br/>" );
+
+
+
+var three =("The value of a++ is :");
+document.write(three + oneresult)
+document.write("<br/>" );
+var three =("The value of a is :")
+
+
+var threenum=(num++);
+
+document.write(three + threenum)
+
